533a4bc2c14cfb0a0356a1321ce0a1593ba24494
dmercel3DUN <31537882+dmercel3DUN@users.noreply.github.com>
2022-02-28 23:38:26 +0000

    Updates

---
+++ precache-manifest.d4a5ef2dee7fecdc37a82b2f6e3bcd5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c72b58f6afcc86ce4dd0",
+    "url": "/monsters-rolodex/static/css/main.51bf4f7f.chunk.css"
+  },
+  {
+    "revision": "c72b58f6afcc86ce4dd0",
+    "url": "/monsters-rolodex/static/js/main.c72b58f6.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/monsters-rolodex/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "4933064c23f62bd41ef2",
+    "url": "/monsters-rolodex/static/js/2.4933064c.chunk.js"
+  },
+  {
+    "revision": "baf071cd605113dee954",
+    "url": "/monsters-rolodex/static/js/runtime~main.baf071cd.js"
+  },
+  {
+    "revision": "403a4b7b5bedc04782b8715d2a34c322",
+    "url": "/monsters-rolodex/index.html"
+  }
+];
